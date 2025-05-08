a9eac73dea2852062d4979199a2c124395af284c
CodeJuuun <Chomseng.a@gmail.com>
2025-05-08 17:14:40 -0400

    added lifecycle methods to dashboard component and also added more props to state object

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -26,31 +26,58 @@ const data = [
   }
 ];
 
+
 class Dashboard extends Component {
 
+  // initial state
   state = {
-    loading: false,
+    focused: null, 
+    loading: true,
+    photos: [],
+    topics: []
   };
 
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
 
+    if (focused) {
+      this.setState({ focused });
+    }
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+  // function expression instance method
+  selectPanel(id) {
+    this.setState(prevState => ({ //takes previous state (or current one) 
+      focused: prevState.focused !== null ? null : id  // take current focused
+    }))
+  }
+  
+
+  // rendering
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", { "dashboard--focused": this.state.focused});
+
     if (this.state.loading) {
       return <Loading />;
     }
 
-
-    const panels = data.map((panel) => {
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data).map(panel => (
       <Panel
         key={panel.id}
-        id={panel.id}
         label={panel.label}
-        value={panel.value} />;
-    });
+        value={panel.value}
+        onSelect={event => this.selectPanel(panel.id)}
+      />
+    ));
 
-    return <main className={dashboardClasses}>{panels}</main> 
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
-
 
 export default Dashboard;
